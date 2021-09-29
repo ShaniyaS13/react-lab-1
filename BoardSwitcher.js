e1b9a557b2fbd57896dbb7c4f424376677157cf4
ShaniyaS13 <57805735+ShaniyaS13@users.noreply.github.com>
2021-09-29 00:10:51 -0400

    Add files via upload

---
+++ BoardSwitcher.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+class Board extends React.Component {
+  render() {
+    let className = "board";
+    if (this.props.selected) {
+      className += " selected";
+    }
+    return (
+      <div className={className}>
+        {this.props.index + 1}
+      </div>
+    );
+  }
+}
+
+class BoardSwitcher extends React.Component {
+
+  constructor(props) {
+    super(props); // required so that props work
+    this.state = {clicks: 0,};
+  }
+
+  handleClick(event) {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+    if(this.state.clicks > 1){
+      this.setState({
+        clicks: 0
+      });
+    }
+  }
+  render() {
+    let boards = [];
+    for (let ii = 0; ii < this.props.numBoards; ii++) {
+      let isSelected = ii === this.state.clicks;
+      boards.push(
+        <Board index={ii} selected={isSelected} key={ii} />
+      );
+    }
+    return (
+      <div>
+        <div className="boards">{boards}</div>
+        <button onClick={ (e) => this.handleClick(e) }>{ this.props.btnText }Toggle</button>
+      </div>
+    );
+  }
+}
+
+export default BoardSwitcher;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
