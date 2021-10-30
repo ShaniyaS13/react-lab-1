973303809e371db9c406e406f14477afa9ac1488
ShaniyaS13 <57805735+ShaniyaS13@users.noreply.github.com>
2021-10-30 17:19:40 -0400

    Add files via upload

--- src/BoardSwitcher.js
+++ src/BoardSwitcher.js
@@ -15,6 +15,73 @@ class Board extends React.Component {
 }
 
 class BoardSwitcher extends React.Component {
+
+  constructor(props) {
+    super(props); // required so that props work
+    this.state = {clicks: 0,};
+  }
+
+  handleClick(event) {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+    if(this.state.clicks > 1){
+      this.setState({
+        clicks: 0
+      });
+    }
+  }
+  render() {
+    let boards = [];
+    for (let ii = 0; ii < this.props.numBoards; ii++) {
+      let isSelected = ii === this.state.clicks;
+      boards.push(
+        <Board index={ii} selected={isSelected} key={ii} />
+      );
+    }
+    return (
+      <div>
+        <div className="boards">{boards}</div>
+        <button onClick={ (e) => this.handleClick(e) }>{ this.props.btnText }Toggle</button>
+      </div>
+    );
+  }
+}
+
+export default BoardSwitcher;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Board extends React.Component {
+  render() {
+    let className = "board";
+    if (this.props.selected) {
+      className += " selected";
+    }
+    return (
+      <div className={className}>
+        {this.props.index + 1}
+      </div>
+    );
+  }
+}
+
+class BoardSwitcher extends React.Component {
   render() {
     let boards = [];
     for (let ii = 0; ii < this.props.numBoards; ii++) {
@@ -23,7 +90,6 @@ class BoardSwitcher extends React.Component {
         <Board index={ii} selected={isSelected} key={ii} />
       );
     }
-
     return (
       <div>
         <div className="boards">{boards}</div>
@@ -33,4 +99,4 @@ class BoardSwitcher extends React.Component {
   }
 }
 
-export default BoardSwitcher;
+export default BoardSwitcher; */
